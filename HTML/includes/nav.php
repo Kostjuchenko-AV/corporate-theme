9aaf599a8f2a52d42bb9e69cad15d2bca609e44c
Nathaniel Deal <info@nathanieldeal.com>
2014-10-15 18:32:44 -0400

    Convert html to php for templating

---
+++ HTML/includes/nav.php
@@ -0,0 +1,90 @@
+<!--Begin Navigation-->
+<div class="navbar-wrapper">
+	<div class="container">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="navbar-header">
+				 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1"> 
+					 <span class="sr-only">Toggle navigation</span>
+					 <i class="fa fa-bars"></i>
+				 </button> 
+				 <!--Begin Logo -->
+				 <a class="navbar-brand" href="#">
+				 	<img src="img/logo.png" title="Amity" alt="Amity" />
+				 </a>
+				 <!--End Logo -->
+			</div>
+			
+			<div class="collapse navbar-collapse" id="navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown active">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							Home <strong class="caret"></strong>
+						</a>
+						<ul class="dropdown-menu animated fadeIn">
+							<li>
+								<a href="#">Dropdown Link</a>
+							</li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							Pages <strong class="caret"></strong>
+						</a>
+						<ul class="dropdown-menu animated fadeIn">
+							<li>
+								<a href="#">Dropdown Link</a>
+							</li>
+							<li>
+								<a href="#">Dropdown Link</a>
+							</li>
+							<li>
+								<a href="#">Dropdown Link</a>
+							</li>
+							<li>
+								<a href="#">Dropdown Link</a>
+							</li>
+						</ul>	
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						 	Features <strong class="caret"></strong>
+						</a>
+						<ul class="dropdown-menu animated fadeIn">
+							<li>
+								<a href="#">Header Options</a>
+								<a href="#">Footer Options</a>
+								<a href="#">Animations</a>
+								<a href="#">Icons</a>
+								<a href="#">Typography</a>
+							</li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							Portfolio <strong class="caret"></strong>
+						</a>
+						<ul class="dropdown-menu animated fadeIn">
+							<li>
+								<a href="#">Dropdown Link</a>
+							</li>
+						</ul>	
+					</li>
+					<li>
+						<a href="#">Blog</a>	
+					</li>
+					<li>
+						<a href="#">Contact</a>	
+					</li>
+				</ul>
+				<!--Search Form
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" />
+					</div> <button type="submit" class="btn btn-default">Submit</button>
+				</form>
+				-->
+			</div>
+		</nav>
+	</div>
+</div>		
+<!--End Navigation-->
