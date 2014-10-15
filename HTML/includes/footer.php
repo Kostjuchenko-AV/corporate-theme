f31ec6e44fc4e73cd0ecf0b6952864cf959b9e6e
Nathaniel Deal <info@nathanieldeal.com>
2014-10-15 19:58:40 -0400

    Fix header issue and add seperate header and footer php

---
+++ HTML/includes/footer.php
@@ -0,0 +1,137 @@
+			<!--Begin Footer Section-->
+			<footer>
+				<div class="footer-top">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-4 contact-block">
+								 <h4 class="title">Contact <span>Us</span></h4>
+								 <ul class="list-unstyled">
+								 	<li>
+								 		<i class="fa fa-map-marker"></i><span>Address:</span> 1170 Peachtree St NE #1200 Atlanta, GA 30039
+								 	</li>
+								 	<li><i class="fa fa-phone"></i><span>Phone:</span> (404) 919 4505</li>
+								 	<li><i class="fa fa-envelope"></i><span>Email:</span> <a href="mailto:hello@nathanieldeal.com">hello@nathanieldeal.com</a></li>
+								 	<li><i class="fa fa-clock-o"></i><span>Monday - Friday:</span> 9:00 am - 6:00 pm <br/>
+								 		<span>Saturday - Sunday:</span> Closed
+								 	</li>
+								 </ul>		
+							</div>
+							<div class="col-sm-4 twitter-block">
+								<h4 class="title">Latest <span>Tweets</span></h4>
+								<ul class="list-unstyled">
+									<li>
+										<span class="fa-stack">
+											<i class="fa fa-circle fa-stack-2x"></i>
+											<i class="fa fa-twitter fa-stack-1x"></i>
+										</span>
+										@NathanielDeal RT Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis laoreet urna. Phasellus nec erat odio. 
+										<time>December 19,2014</time>
+									</li>
+									<li>
+										<span class="fa-stack">
+											<i class="fa fa-circle fa-stack-2x"></i>
+											<i class="fa fa-twitter fa-stack-1x"></i>
+										</span>
+										@NathanielDeal RT Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis laoreet urna.
+										<time>December 19,2014</time>
+									</li>
+								</ul>
+							</div>
+							<div class="col-sm-4 newsletter-block">
+								<h4 class="title">Newsletter</h4>
+								<p>Sign up to recieve our email newsletter to latest news, updates, and specials that we have to offer.</p>
+								<form class="form-inline clearfix" role="form">
+									<div class="form-group">
+										<label for="newsletter-email" class="control-label">Email</label>
+										<input type="email" class="form-control pull-left" id="newsletter-email" />
+										<button type="submit" class="btn btn-primary">Subscribe</button>
+									</div>
+								</form>
+								<small>Trust us, we hate spam as much as you do.</small>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="footer-bottom">
+					<div class="container">	
+						<div class="row">
+							<div class="col-sm-8">
+								 <p>&copy; Copyright 2014 Amity. All Rights Reserved. Designed by Nathaniel Deal</p>
+							</div>
+							<div class="col-sm-4">
+								<div class="follow pull-right">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+									<a href="#"><i class="fa fa-yelp"></i></a>
+									<a href="#"><i class="fa fa-youtube"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>		
+			</footer>	
+
+		<!--End Wrapper-->
+		</div>
+	
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="bower_components/jquery/dist/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="bower_components/jquery-waypoints/waypoints.min.js"></script>
+		<script src="bower_components/jquery-waypoints/shortcuts/sticky-elements/waypoints-sticky.min.js"></script>
+		<script src="bower_components/stellar.js/jquery.stellar.min.js"></script>
+		<script src="bower_components/jquery-prettyPhoto/js/jquery.prettyPhoto.js"></script>
+		<script>
+				
+			// Stick Header Top Top
+			$('.navbar-wrapper').waypoint('sticky');
+	
+			// Shrink Header
+			$('.cta').waypoint(function(direction) {
+			  $('header').toggleClass('shrink-header', direction === 'down');
+			}, { offset: 108 });
+				
+			// Initialize Main Carousel
+			$('.carousel').carousel({
+				interval: 15000
+			});
+		 	
+		 	// Parallax Background Effect
+		 	$.stellar({
+				horizontalScrolling: false,
+				verticalOffset: 40
+			});
+			
+			// Initialize PrettyPhoto for gallery
+			$("a[rel^='prettyPhoto']").prettyPhoto({
+				theme: 'facebook',
+				slideshow:5000, 
+				autoplay_slideshow:false,
+				show_title: false,
+				theme: 'light_square',
+				social_tools: false,
+				overlay_gallery: false 
+			});
+			
+			// Expand Portfolio Button
+			
+			$('.toggle-portfolio').click(function(event) {
+				event.preventDefault();
+				$("a[rel^='prettyPhoto']:first").click();
+			});
+			
+			// Animate Progress Bars
+			
+			$('.services').waypoint(function() {
+				$('.graph-item .animated-bar').each(function(){
+					var setWidth = $(this).attr('aria-valuenow');
+					$(this).width(setWidth + '%');
+				});
+			}, { offset: 400 });
+			
+		</script>
+		
+	</body>
+</html>
---
+++ HTML/includes/header.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Meta Data -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="author" content="Nathaniel Deal">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Preview Of Amity - Responsive Website Template For WrapBootstrap">
+		<title>Amity - Responsive Bootstrap Template For WrapBootstrap</title>
+		
+		<!-- CSS -->
+		<link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+		<link href="bower_components/animate.css/animate.min.css" rel="stylesheet">
+		<link href="bower_components/jquery-prettyPhoto/css/prettyPhoto.css" rel="stylesheet">
+		<link href="css/main.css" rel="stylesheet">
+		
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<!--Begin Wrapper-->
+		<div id="wrapper">
+			
+			<!--Begin Header-->
+			<header>	
+				<div id="top">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12 clearfix">
+								<div class="contact pull-left">
+									<span><i class="fa fa-phone"></i><address> Call Us: (404) 919 4505</address></span>	
+									<span class="hidden-xs"><i class="fa fa-envelope"></i><address> <a href="mailto:hello@nathanieldeal.com">Email: hello@nathanieldeal.com</a> </address></span>			
+								</div>
+								<div class="follow pull-right hidden-xs">
+									<span>Follow Us:</span>
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+									<a href="#"><i class="fa fa-yelp"></i></a>
+									<a href="#"><i class="fa fa-youtube"></i></a>
+								</div>
+							</div>
+						</div>	
+					</div>	
+				</div>
+				
+				<!--Begin Navigation-->
+				<div class="navbar-wrapper">
+			    	<div class="container">
+						<nav class="navbar navbar-default" role="navigation">
+							<div class="navbar-header">
+								 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1"> 
+									 <span class="sr-only">Toggle navigation</span>
+									 <i class="fa fa-bars"></i>
+								 </button> 
+								 <!--Begin Logo -->
+								 <a class="navbar-brand" href="#">
+								 	<img src="img/logo.png" title="Amity" alt="Amity" />
+								 </a>
+								 <!--End Logo -->
+							</div>
+							
+							<div class="collapse navbar-collapse" id="navbar-collapse-1">
+								<ul class="nav navbar-nav navbar-right">
+									<li class="dropdown active">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+											Home <strong class="caret"></strong>
+										</a>
+										<ul class="dropdown-menu animated fadeIn">
+											<li>
+												<a href="#">Dropdown Link</a>
+											</li>
+										</ul>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+											Pages <strong class="caret"></strong>
+										</a>
+										<ul class="dropdown-menu animated fadeIn">
+											<li>
+												<a href="#">Dropdown Link</a>
+											</li>
+											<li>
+												<a href="#">Dropdown Link</a>
+											</li>
+											<li>
+												<a href="#">Dropdown Link</a>
+											</li>
+											<li>
+												<a href="#">Dropdown Link</a>
+											</li>
+										</ul>	
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+										 	Features <strong class="caret"></strong>
+										</a>
+										<ul class="dropdown-menu animated fadeIn">
+											<li>
+												<a href="#">Header Options</a>
+												<a href="#">Footer Options</a>
+												<a href="#">Animations</a>
+												<a href="#">Icons</a>
+												<a href="#">Typography</a>
+											</li>
+										</ul>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+											Portfolio <strong class="caret"></strong>
+										</a>
+										<ul class="dropdown-menu animated fadeIn">
+											<li>
+												<a href="#">Dropdown Link</a>
+											</li>
+										</ul>	
+									</li>
+									<li>
+										<a href="#">Blog</a>	
+									</li>
+									<li>
+										<a href="#">Contact</a>	
+									</li>
+								</ul>
+								<!--Search Form
+								<form class="navbar-form navbar-left" role="search">
+									<div class="form-group">
+										<input type="text" class="form-control" />
+									</div> <button type="submit" class="btn btn-default">Submit</button>
+								</form>
+								-->
+							</div>
+						</nav>
+			    	</div>
+				</div>		
+				<!--End Navigation-->
+			</header>	
+			<!--End Header-->	
