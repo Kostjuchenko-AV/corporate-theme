433f899583e3ec6694464486aa49f690963d4ba8
Nathaniel Deal <info@nathanieldeal.com>
2014-10-17 16:12:52 -0400

    Add right sidebar page

---
+++ HTML/page-right-sidebar.php
@@ -0,0 +1,73 @@
+		<?php include('includes/header.php'); ?>
+			
+			<!--Begin Title Bar-->
+			<section class="title-bar">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<ul class="breadcrumb">
+								<li><a href="/">Home</a></li>
+								<li class="active">Pages</li>
+							</ul>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<h2>Right Sidebar Page</h2>
+						</div>
+					</div>
+				</div>
+			</section>			
+			<!--End Title Bar-->
+			
+				
+			<div class="container">
+				<div class="row">
+										
+					<!--Begin Main Section-->	
+					<div class="col-md-9">
+						<div class="row">
+							<div class="col-md-12">
+								<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla non pulvinar. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut eu risus enim, ut pulvinar lectus. Sed hendrerit nibh metus.
+								</p>								
+								<p><img class="pull-left img-responsive padded" src="img/responsive-illustrations.png" alt="Responsive devices">Lorem ipsum dolor sit amet, <a href="#">consectetur</a> adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								
+								<p>Lorem ipsum dolor sit amet, <a href="#">consectetur</a> adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+								
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+							</div>	
+						</div>
+					</div>
+					<!--End Main Section-->	
+					
+					<!--Begin Right Sidebar-->
+					<div class="col-md-3">
+						<aside class="sidebar">
+							<h4>Side Menu</h4>
+							<ul class="nav nav-list">
+								<li><a href="#"><i class="fa fa-caret-right"></i> Design</a></li>
+								<li><a href="#"><i class="fa fa-caret-right"></i> Photos</a></li>
+								<li><a href="#"><i class="fa fa-caret-right"></i> Videos</a></li>
+								<li><a href="#"><i class="fa fa-caret-right"></i> Lifestyle</a></li>
+								<li><a href="#"><i class="fa fa-caret-right"></i> Technology</a></li>
+							</ul>
+							<hr>
+							<h4>Youtube Video</h4>
+							<div class="responsive-video">
+								<iframe width="560" height="315" src="//www.youtube.com/embed/hbaTnin7GFE?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+							</div>
+							<hr>
+							<h4>Text Box</h4>
+							<p>Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Nulla nunc dui, tristique in semper vel. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero.</p>
+						</aside>
+					</div>
+					<!--End Right Sidebar-->
+
+				</div>
+			</div>
+			
+			
+		<?php include('includes/footer.php'); ?>
+			
