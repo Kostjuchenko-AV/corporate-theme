e0e8bb4733efe604b9db7e13d1c1d26a18d272eb
Nathaniel Deal <info@nathanieldeal.com>
2014-10-24 16:12:22 -0400

    Fix IE8 issues and replace jquery version

--- HTML/includes/footer.php
+++ HTML/includes/footer.php
@@ -109,7 +109,7 @@
 		<!--End Style Switcher -->
 	
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="bower_components/jquery/dist/jquery.min.js"></script>
+		<script src="js/jquery-1.11.1.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 		<script src="bower_components/jquery-waypoints/waypoints.min.js"></script>
@@ -121,6 +121,7 @@
 		<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
 		<script src="bower_components/gmaps/gmaps.js"></script>
 		<script src="js/main.js"></script>
+		<script src="js/maps.js"></script>
 		
 	</body>
 </html>
